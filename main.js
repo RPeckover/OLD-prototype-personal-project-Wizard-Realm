370594cd3e81a1dd984a9f4db52a5c51207a743d
RPeckover <Henry.peckover19@bathspa.ac.uk>
2026-01-18 17:28:05 +0000

    Old wizard site prototype
    Old wizard website prototype

---
+++ main.js
@@ -0,0 +1,17 @@
+let userInput;
+
+let fortunes = [
+    "example", 
+    "example2",
+];
+ 
+// switch(userInput) {
+    
+// }
+
+if (userInput.includes())
+
+function randomFortune() {
+    console.log(fortunes[(Math.floor(Math.random() * arr.length))]);
+}
+randomFortune();
